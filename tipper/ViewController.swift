01ba9dd938ac59e3ad5a59db514766adb6d776ce
Ali Afridi <afridi2@illinois.edu>
2015-12-24 17:23:59 -0700

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Ali Afridi on 12/24/15.
+//  Copyright (c) 2015 Ali Afridi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
